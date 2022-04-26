dc0f6ff8057082c8cf6d07d6ba237b2543902b20
Danila Manakov <manakov.danila@mail.ru>
2022-04-26 07:12:50 +0300

    add: m3i

--- manakov_d_d/prj.lab/m3i/m3i.cpp
+++ manakov_d_d/prj.lab/m3i/m3i.cpp
@@ -1,99 +1,238 @@
-#include <cassert>
+#include <cstring>
+#include <iostream>
 #include <m3i/m3i.h>
 #include <stdexcept>
-#include <utility>
 
-inline void unimplemented() { assert(false); }
+inline int min(const int a, const int b) { return (a < b) ? a : b; }
+
+typedef const std::lock_guard<std::mutex> lock_guard;
 
 M3i::M3i() = default;
 
-M3i::M3i(int d0, int d1, int d2)
-    : data(new int[d0 * d1 * d2]()), dim0(d0), dim1(d1), dim2(d2),
-      ref_counter(new int(1)) {}
+// {{{123}, {123}}, {{123}, {123}}, {{123}, {123}}, ...}
+M3i::M3i(
+    std::initializer_list<std::initializer_list<std::initializer_list<int>>>
+        ilist) {
+  tensor = new Tensor();
+  tensor->shape[0] = ilist.size();
+  tensor->shape[1] = ilist.begin()->size();
+  tensor->shape[2] = ilist.begin()->begin()->size();
+  tensor->capacity = tensor->shape[0] * tensor->shape[1] * tensor->shape[2];
+  tensor->data = new int[tensor->capacity];
 
-M3i::M3i(const M3i &other) {
-  if (data != nullptr) {
-    delete[] data;
-    delete ref_counter;
+  int dumb_iter = 0;
+  for (auto &sublist : ilist) {
+    for (auto &subsublist : sublist) {
+      for (auto &value : subsublist) {
+        tensor->data[dumb_iter++] = value;
+      }
+    }
   }
-
-  data = other.data;
-  ref_counter = other.ref_counter;
-  dim0 = other.dim0;
-  dim1 = other.dim1;
-  dim2 = other.dim2;
 }
 
-M3i::M3i(M3i &&other) {
-  if (data != nullptr) {
-    delete[] data;
-    delete ref_counter;
-  }
-
-  data = std::move(other.data);
-  ref_counter = std::move(other.ref_counter);
-  dim0 = std::move(other.dim0);
-  dim1 = std::move(other.dim1);
-  dim2 = std::move(other.dim2);
+M3i::M3i(int d0, int d1, int d2) {
+  tensor = new Tensor();
+  tensor->shape[0] = d0;
+  tensor->shape[1] = d1;
+  tensor->shape[2] = d2;
+  tensor->capacity = d0 * d1 * d2;
+  CheckDims();
+  tensor->data = new int[tensor->capacity]();
 }
 
-M3i &M3i::operator=(M3i &&other) {
-  *this = std::move(other);
+M3i::M3i(int d0, int d1, int d2, int fill) {
+  tensor = new Tensor();
+  tensor->shape[0] = d0;
+  tensor->shape[1] = d1;
+  tensor->shape[2] = d2;
+  tensor->capacity = d0 * d1 * d2;
+  CheckDims();
+  tensor->data = new int[tensor->capacity]();
+
+  for (int i = 0; i < tensor->capacity; ++i)
+    tensor->data[i] = fill;
+}
+
+// легкая копия(подсчет ссылок)
+M3i::M3i(const M3i &other) : tensor(other.tensor) { ++tensor->ref_counter; }
+
+// легкая копия(подсчет ссылок)
+M3i &M3i::operator=(const M3i &other) {
+  free();
+  tensor = other.tensor;
+  ++tensor->ref_counter;
   return *this;
 }
 
-M3i::~M3i() {
-  if (data != nullptr && *ref_counter == 1) {
-    delete[] data;
-    delete ref_counter;
+M3i::M3i(M3i &&other) : tensor(other.tensor) { other.tensor = nullptr; }
+
+M3i &M3i::operator=(M3i &&other) {
+  if (this == &other) {
+    return *this;
   }
+
+  free();
+  tensor = other.tensor;
+  other.tensor = nullptr;
+  return *this;
 }
 
-const M3i M3i::Clone() const {
-  M3i ret(dim0, dim1, dim2);
-  for (int i = 0; i < dim0 * dim1 * dim2; ++i) {
-    ret.data[i] = data[i];
-  }
+M3i::~M3i() { free(); }
 
+// полная копия данных
+M3i M3i::Clone() const {
+  lock_guard(tensor->mutex);
+  M3i ret(tensor->shape[0], tensor->shape[1], tensor->shape[2]);
+
+  memcpy(ret.tensor->data, tensor->data, tensor->capacity);
   return ret;
 }
 
+// except: хотя бы один из индексов меньше нуля или больше
+// соответствующей размерности
 M3i &M3i::Resize(int d0, int d1, int d2) {
-  unimplemented();
-  throw 0;
+  CheckDims(d0, d1, d2);
+  lock_guard(tensor->mutex);
+  int *buffer = new int[d0 * d1 * d2]();
+
+  for (int i = 0; i < min(tensor->shape[0], d0); ++i) {
+    for (int j = 0; j < min(tensor->shape[1], d1); ++j) {
+      for (int k = 0; k < min(tensor->shape[2], d2); ++k) {
+        buffer[i * d1 * d2 + j * d2 + k] = this->At(i, j, k);
+      }
+    }
+  }
+
+  delete[] tensor->data;
+  tensor->shape[0] = d0;
+  tensor->shape[1] = d1;
+  tensor->shape[2] = d2;
+  tensor->capacity = d0 * d1 * d2;
+  tensor->data = buffer;
+
+  return *this;
 }
 
+// except: хотя бы один из индексов меньше нуля или больше
+// соответствующей размерности
 int &M3i::At(int i, int j, int k) {
-  if (i >= dim0 || j >= dim1 || k >= dim2) {
-    throw std::out_of_range("out of range");
-  }
-
-  return data[i * (dim1 * dim2) + j * dim2 + k];
+  CheckIndexes(i, j, k);
+  return tensor->data[i * tensor->shape[1] * tensor->shape[2] +
+                      j * tensor->shape[2] + k];
 }
 
+// except: хотя бы один из индексов меньше нуля или больше
+// соответствующей размерности
 int M3i::At(int i, int j, int k) const {
-  if (i >= dim0 || j >= dim1 || k >= dim2) {
-    throw std::out_of_range("out of range");
-  }
-
-  return data[i * (dim1 * dim2) + j * dim2 + k];
+  CheckIndexes(i, j, k);
+  return tensor->data[i * tensor->shape[1] * tensor->shape[2] +
+                      j * tensor->shape[2] + k];
 }
 
+// получение размера по измерению 0, 1, 2
+// except: dim < 0 or dim > 2
 int M3i::Size(int dim) const {
-  if (dim < 1 || dim > 3) {
-    throw std::out_of_range("out of range");
+  if (dim < 0 or dim > 2) {
+    throw std::invalid_argument("dim must be between 0 and 2");
   }
-
-  if (dim == 1) {
-    return dim0;
-  } else if (dim == 2) {
-    return dim1;
-  }
-  return dim2;
+  return tensor->shape[dim];
 }
 
-void M3i::Fill(int value) {
-  for (int i = 0; i < dim0 * dim1 * dim2; ++i) {
-    data[i] = value;
+// заполнение одним и тем же значением
+void M3i::Fill(int val) {
+  lock_guard(tensor->mutex);
+  for (int i = 0; i < tensor->capacity; ++i)
+    tensor->data[i] = val;
+}
+
+std::ostream &M3i::WriteTo(std::ostream &out) const {
+  lock_guard(tensor->mutex);
+
+  out << "size: " << tensor->shape[0] << " " << tensor->shape[1] << " "
+      << tensor->shape[2] << std::endl;
+  for (int i = 0; i < tensor->shape[0]; ++i) {
+    for (int j = 0; j < tensor->shape[1]; ++j) {
+      for (int k = 0; k < tensor->shape[2]; ++k) {
+        out << this->At(i, j, k) << " ";
+      }
+      out << std::endl;
+    }
+    out << std::endl;
   }
+  return out;
+}
+
+std::istream &M3i::ReadFrom(std::istream &in) {
+  if (tensor != nullptr) {
+    lock_guard(tensor->mutex);
+    free();
+  }
+
+  std::string must_be_size_string;
+  in >> must_be_size_string;
+  if (must_be_size_string != "size:") {
+    in.setstate(std::ios_base::failbit);
+    return in;
+  }
+
+  int dims[3];
+  in >> dims[0] >> dims[1] >> dims[2];
+
+  if (dims[0] <= 0 || dims[1] <= 0 || dims[2] <= 0) {
+    in.setstate(std::ios_base::failbit);
+    return in;
+  }
+
+  M3i new_me = M3i(dims[0], dims[1], dims[2]);
+
+  for (int i = 0; i < dims[0]; ++i) {
+    for (int j = 0; j < dims[1]; ++j) {
+      for (int k = 0; k < dims[2]; ++k) {
+        in >> new_me.tensor->data[i * dims[1] * dims[2] + j * dims[2] + k];
+      }
+    }
+  }
+
+  *this = new_me;
+
+  return in;
+}
+
+std::istream &operator>>(std::istream &in, M3i &m) { return m.ReadFrom(in); }
+
+std::ostream &operator<<(std::ostream &out, const M3i &m) {
+  return m.WriteTo(out);
+}
+
+void M3i::CheckDims() {
+  if (tensor->shape[0] <= 0 or tensor->shape[1] <= 0 or tensor->shape[2] <= 0) {
+    throw std::invalid_argument("dimensions must be above zero");
+  }
+}
+
+void M3i::CheckDims(int d0, int d1, int d2) const {
+  if (d0 <= 0 or d1 <= 0 or d2 <= 0) {
+    throw std::invalid_argument("dimensions must be above zero");
+  }
+}
+
+void M3i::CheckIndexes(int d0, int d1, int d2) const {
+  if (d0 < 0 or d1 < 0 or d2 < 0 or tensor->shape[0] <= d0 or
+      tensor->shape[1] <= d1 or tensor->shape[2] <= d2) {
+    throw std::out_of_range("index(es) out of range");
+  }
+}
+
+void M3i::free() {
+  if (tensor == nullptr)
+    return;
+
+  lock_guard(tensor->mutex);
+
+  if (--tensor->ref_counter == 0) {
+    delete[] tensor->data;
+    delete tensor;
+  }
+
+  tensor = nullptr;
 }
--- manakov_d_d/prj.lab/m3i/m3i.h
+++ manakov_d_d/prj.lab/m3i/m3i.h
@@ -1,14 +1,26 @@
 #ifndef M3I_H
 #define M3I_H
 
+#include <atomic>
+#include <initializer_list>
 #include <iosfwd>
+#include <memory>
+#include <mutex>
 
 class M3i {
 public:
   M3i();
-  M3i(int d1, int d2, int d3);
+
+  // {{{123}, {123}}, {{123}, {123}}, {{123}, {123}}, ...}
+  M3i(std::initializer_list<std::initializer_list<std::initializer_list<int>>>);
+
+  // except: хотя бы один из индексов меньше нуля
+  M3i(int d0, int d1, int d2); // незаполняющий конструктор
+  M3i(int d0, int d1, int d2, int fill_with); // заполняющий конструктор
+
   // легкая копия(подсчет ссылок)
   M3i(const M3i &other);
+
   // легкая копия(подсчет ссылок)
   M3i &operator=(const M3i &other);
 
@@ -18,32 +30,50 @@ public:
   ~M3i();
 
   // полная копия данных
-  const M3i Clone() const;
+  M3i Clone() const;
 
-  M3i &Resize(int d1, int d2, int d3);
+  // except: хотя бы один из индексов меньше нуля или больше
+  // соответствующей размерности
+  M3i &Resize(int d0, int d1, int d2);
 
-  // доступ к элементу
+  // except: хотя бы один из индексов меньше нуля или больше
+  // соответствующей размерности
   int &At(int i, int j, int k);
+
+  // except: хотя бы один из индексов меньше нуля или больше
+  // соответствующей размерности
   int At(int i, int j, int k) const;
 
   // получение размера по измерению 0, 1, 2
+  // except: dim < 0 or dim > 2
   int Size(int dim) const;
-  // заполнение значениями
+
+  // заполнение одним и тем же значением
   void Fill(int val);
 
-  std::ostream &WriteTo(std::ostream &ostrm) const;
-  std::istream &ReadFrom(std::istream &istrm);
+  std::ostream &WriteTo(std::ostream &) const;
+  std::istream &ReadFrom(std::istream &);
 
 private:
-  int dim0;
-  int dim1;
-  int dim2;
+  void CheckDims();
 
-  int *data;
-  int *ref_counter;
+  void CheckDims(int d0, int d1, int d2) const;
+
+  void CheckIndexes(int i, int j, int k) const;
+
+  void free();
+
+  struct Tensor {
+    int shape[3] = {0};
+    int capacity = 0;
+    int *data = nullptr;
+    std::mutex mutex;
+    std::atomic_int16_t ref_counter = 1;
+  } *tensor = nullptr;
 };
 
-std::istream &operator>>(std::istream &istrm, M3i &m);
-std::ostream &operator<<(std::ostream &ostrm, const M3i &m);
+std::istream &operator>>(std::istream &, M3i &);
+
+std::ostream &operator<<(std::ostream &, const M3i &);
 
 #endif // M3I_H
